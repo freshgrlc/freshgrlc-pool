aff5ed4f5a3571878cb8d0387392890be244a569
Erik van `t Wout <erik@ewnetworks.eu>
2020-05-17 22:39:31 +0200

    * [socket] Make ListeningConnectionManager more useful by including a listener socket and listener thread within it

--- lib/socket/ListeningConnectionManager.cpp
+++ lib/socket/ListeningConnectionManager.cpp
@@ -16,3 +16,11 @@ int ListeningConnectionManager::listen()
     mlog(INFO, "Opened listener on port %d", _listener.port());
     return _listener.listen(ConnectionManager::addConnection, this);
 }
+
+void ListeningConnectionManager::ListenerThread::main()
+{
+    /* Should never return */
+    int errorCode = this->parent.listen();
+
+    throw std::runtime_error("Failed to listen on stratum listener socket: Error " + std::to_string(errorCode));
+}
--- lib/socket/ListeningConnectionManager.h
+++ lib/socket/ListeningConnectionManager.h
@@ -3,19 +3,35 @@
 
 #include "ConnectionManager.h"
 
+#include <util/Thread.h>
+
+
 class ListeningConnectionManager : public ConnectionManager
 {
     public:
-        inline ListeningConnectionManager(Listener &listener) : ConnectionManager(),
-            _listener(listener)
+        inline ListeningConnectionManager(Listener &&listener) : ConnectionManager(),
+            _listener(std::move(listener)),
+            _listenerThread(*this)
         {}
 
-        int listen(void);
-
-        inline Listener &listener(void)                                         { return _listener; }
+        inline Listener &listener(void)             { return _listener; }
+        inline void startProcessing(void)           { _listenerThread.start(); }
 
     private:
-        Listener &_listener;
+        class ListenerThread : public Thread
+        {
+            public:
+                inline ListenerThread(ListeningConnectionManager &parent) : parent(parent) {}
+                void main(void) override;
+
+            private:
+                ListeningConnectionManager &parent;
+        };
+
+        Listener _listener;
+        ListenerThread _listenerThread;
+
+        int listen(void);
 };
 
 #endif
